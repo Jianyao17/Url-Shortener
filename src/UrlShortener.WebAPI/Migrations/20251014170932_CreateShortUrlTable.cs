d7c370b390dcfa6e99ac4eecefa553dfd542e192
Kagayaku Kagami <jianyao2555@gmail.com>
2025-10-15 02:03:15 +0700

    Initial Commit

---
+++ src/UrlShortener.WebAPI/Migrations/20251014170932_CreateShortUrlTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateShortUrlTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShortUrls",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ShortCode = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    OriginalUrl = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortUrls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_ShortCode",
+                table: "ShortUrls",
+                column: "ShortCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShortUrls");
+        }
+    }
+}
